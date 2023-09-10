b74de4f135dd91fc1c4e1e4f33353d6bdffdd0ff
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2023-09-10 14:11:35 +0600

    First Activity Second Semester

--- Activity One Second Semestr/src/Designer.java
+++ Activity One Second Semestr/src/Designer.java
@@ -1,2 +1,20 @@
-public class Designer {
+public class Designer extends Profession {
+    String name;
+    int age;
+    int salary;
+    String activity;
+    public Designer(String name, int age, int salary, String activity){
+        super(activity, Designer.class.getName());
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.activity = activity;
+    }
+    @Override
+    public void Action(){
+        super.Action();
+    }
+    public void getInfo(){
+        System.out.printf("%s is %s. Age: %s. Salary: %s$ \n", name, Profession.class.getName(), age, salary);
+    }
 }
--- Activity One Second Semestr/src/Main.java
+++ Activity One Second Semestr/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Teacher Elena = new Teacher("Elena", 26, 6000, "teach students");
+        Teacher Elena = new Teacher("Elena", 26, 6000, "teaches students");
         Elena.getInfo();
         Elena.Action();
+        Designer David = new Designer("David", 21, 20000, "makes things prettier");
+        David.getInfo();
+        David.Action();
     }
 }
